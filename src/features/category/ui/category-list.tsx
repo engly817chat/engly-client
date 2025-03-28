a1bccc0e9df88481c591b943cb536f392593b47f
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-03-28 18:56:24 +0200

    feat: fetch categories from server instead of using mock data

--- src/features/category/ui/category-list.tsx
+++ src/features/category/ui/category-list.tsx
@@ -1,47 +1,98 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { categories } from '../model'
+import { AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+import { Skeleton } from '@/shared/ui/common/skeleton'
+import { Category, categoryApi } from '../model'
 
 export const CategoryList = () => {
+  const [categories, setCategories] = useState<Category[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    const fetchCategories = async () => {
+      try {
+        setLoading(true)
+        const data = await categoryApi.getAllCategories({ signal: controller.signal })
+        setCategories(data.categories)
+      } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+          toast.error(error.message)
+        } else if (error instanceof Error) {
+          toast.error(error.message)
+        } else {
+          toast.error('An unexpected error occurred')
+        }
+        console.error(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCategories()
+
+    return () => controller.abort()
+  }, [])
+
   return (
     <div className='grid grid-cols-2 gap-5 md:grid-cols-3 xl:grid-cols-4 xl:gap-6'>
-      {categories.map(cat => (
-        <Link
-          href={cat.title.toLowerCase()}
-          key={cat.id}
-          className='h-[102px] rounded-[10px] border border-border bg-white p-3 hover:border-primary md:h-[178px] md:space-y-5 md:p-5'
-        >
-          <div className='hidden items-center justify-between md:flex'>
-            <Image src={cat.iconSrc} alt={cat.title} height={24} className='w-auto' />
+      {loading
+        ? Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className='h-[178] w-[306]' />
+          ))
+        : categories.map(cat => {
+            const isTopPick = cat.activeRoomsCount > 20
 
-            {cat.isTop ? (
-              <span className='flex items-center justify-center rounded-[10px] bg-primary px-4 py-1.5 text-sm/none text-primary-foreground'>
-                Top Pick
-              </span>
-            ) : (
-              <span className='flex w-24 items-center justify-center rounded-[10px] bg-[#F6FEFF] py-1.5 text-sm/none'>
-                {cat.count} chats
-              </span>
-            )}
-          </div>
+            return (
+              <Link
+                href={cat.name.toLowerCase()}
+                key={cat.id}
+                className='h-[102px] rounded-[10px] border border-border bg-white p-3 hover:border-primary md:h-[178px] md:space-y-5 md:p-5'
+              >
+                <div className='hidden items-center justify-between md:flex'>
+                  <Image
+                    src={`/icons/${cat.icon}`}
+                    alt={cat.name}
+                    height={24}
+                    width={24}
+                    className='w-auto'
+                  />
 
-          <div className='space-y-2 md:space-y-3'>
-            <h2 className='flex items-center justify-between text-base/5 font-semibold md:text-xl/none'>
-              {cat.title}
+                  {isTopPick ? (
+                    <span className='flex items-center justify-center rounded-[10px] bg-primary px-4 py-1.5 text-sm/none text-primary-foreground'>
+                      Top Pick
+                    </span>
+                  ) : (
+                    <span className='flex w-24 items-center justify-center rounded-[10px] bg-[#F6FEFF] py-1.5 text-sm/none'>
+                      {cat.activeRoomsCount} chats
+                    </span>
+                  )}
+                </div>
 
-              <Image
-                src={cat.iconSrc}
-                alt={cat.title}
-                height={24}
-                className='w-auto md:hidden'
-              />
-            </h2>
-            <p className='line-clamp-2 h-[34px] text-sm/4 md:text-base/none'>
-              {cat.desc}
-            </p>
-          </div>
-        </Link>
-      ))}
+                <div className='space-y-2 md:space-y-3'>
+                  <h2 className='flex items-center justify-between text-base/5 font-semibold md:text-xl/none'>
+                    {cat.name}
+
+                    <Image
+                      src={`/icons/${cat.icon}`}
+                      width={24}
+                      height={24}
+                      alt={cat.name}
+                      className='w-auto md:hidden'
+                    />
+                  </h2>
+                  <p className='line-clamp-2 h-[34px] text-sm/4 md:text-base/none'>
+                    {cat.description}
+                  </p>
+                </div>
+              </Link>
+            )
+          })}
     </div>
   )
 }
