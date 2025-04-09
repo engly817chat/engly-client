416ac5f63618d490886a91e6ec891f38e963407c
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-04-09 21:30:19 +0300

    fix: add close (X) button to registration and login forms

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,35 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { XIcon } from 'lucide-react'
 import { AuthSlider } from '@/features/auth'
+import { Button } from '@/shared/ui/common/button'
 
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const router = useRouter()
+
+  const closeForm = () => {
+    router.push('/')
+  }
+
   return (
     <main className='flex h-screen items-center justify-center bg-slate-800'>
       <div className='flex h-full w-full bg-slate-400 xl:h-[752px] xl:w-[1030px] xl:overflow-hidden xl:rounded-2xl'>
         <AuthSlider />
-        <div className='flex-1'>{children}</div>
+        <div className='relative flex-1'>
+          <Button
+            variant='ghost'
+            onClick={closeForm}
+            className='absolute right-2 top-5 z-10 p-2 text-foreground hover:bg-black/10'
+          >
+            <XIcon className='h-4 w-4' />
+          </Button>
+          {children}
+        </div>
       </div>
     </main>
   )
