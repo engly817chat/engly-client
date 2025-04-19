67b6c0430b85c9c73ac3ae1d0a49aa39619eedb6
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-04-19 13:54:18 +0300

    fix: re-export i18n via shared/lib/index.ts to comply with boundaries rules

--- src/app/providers/I18nProvider.tsx
+++ src/app/providers/I18nProvider.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import '@shared/lib/i18n'
 import { ReactNode } from 'react'
 import { I18nextProvider } from 'react-i18next'
-import i18n from '@shared/lib/i18n'
+import { i18n } from '@shared/lib'
 
 export default function I18nProvider({ children }: { children: ReactNode }) {
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
