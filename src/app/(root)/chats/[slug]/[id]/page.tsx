3ebb3055450c1575e2ef448bd37b6800ef000ff6
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-06-28 17:08:30 +0300

    feat: add AccessGuard to ChatPage

--- src/app/(root)/chats/[slug]/[id]/page.tsx
+++ src/app/(root)/chats/[slug]/[id]/page.tsx
@@ -6,7 +6,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { MoreVertical, Paperclip, Search, Send } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 import { MessagesList, useChatSocket } from '@/features/chats'
-import { useAuth } from '@/entities/auth'
+import { AccessGuard, useAuth } from '@/entities/auth'
 import { Chat, chatsApi, Message } from '@/entities/chats'
 import { Button } from '@/shared/ui/common/button'
 
@@ -68,64 +68,66 @@ export default function ChatPage() {
   }, [chats, chatId])
 
   return (
-    <div className='flex h-full w-full flex-col'>
-      <header className='sticky top-0 z-10 flex items-center justify-between border-b bg-primary-foreground px-6 py-4 shadow-sm'>
-        <div className='ml-4 flex flex-col gap-y-1'>
-          <div className='text-sm font-medium text-foreground md:text-xl'>
-            {isLoading ? (
-              <div className='h-6 w-40 animate-pulse rounded bg-gray-200'></div>
-            ) : (
-              chat?.name || t('chatPage.chatNotFound')
-            )}
+    <AccessGuard requireAuth>
+      <div className='flex h-full w-full flex-col'>
+        <header className='sticky top-0 z-10 flex items-center justify-between border-b bg-primary-foreground px-6 py-4 shadow-sm'>
+          <div className='ml-4 flex flex-col gap-y-1'>
+            <div className='text-sm font-medium text-foreground md:text-xl'>
+              {isLoading ? (
+                <div className='h-6 w-40 animate-pulse rounded bg-gray-200'></div>
+              ) : (
+                chat?.name || t('chatPage.chatNotFound')
+              )}
+            </div>
+            <div className='text-xs text-muted'>
+              {isLoading ? (
+                <div className='h-5 w-16 animate-pulse rounded bg-gray-200'></div>
+              ) : (
+                t('chatPage.members', { count: chat?.chatParticipants.length })
+              )}
+            </div>
           </div>
-          <div className='text-xs text-muted'>
-            {isLoading ? (
-              <div className='h-5 w-16 animate-pulse rounded bg-gray-200'></div>
-            ) : (
-              t('chatPage.members', { count: chat?.chatParticipants.length })
-            )}
+
+          <div className='flex items-center gap-2 md:gap-8'>
+            <div className='flex items-center gap-1 md:gap-2'>
+              <button className='p-1 text-foreground'>
+                <Search size={20} />
+              </button>
+              <button className='p-1 text-foreground'>
+                <MoreVertical size={20} />
+              </button>
+            </div>
           </div>
-        </div>
+        </header>
 
-        <div className='flex items-center gap-2 md:gap-8'>
-          <div className='flex items-center gap-1 md:gap-2'>
-            <button className='p-1 text-foreground'>
-              <Search size={20} />
-            </button>
-            <button className='p-1 text-foreground'>
-              <MoreVertical size={20} />
-            </button>
-          </div>
-        </div>
-      </header>
+        <MessagesList messages={messages} currentUserId={userId} />
 
-      <MessagesList messages={messages} currentUserId={userId} />
+        <div className='flex px-4 pb-8 md768:px-6 md:px-12'>
+          <div className='relative flex-1'>
+            <input
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              onKeyDown={e => e.key === 'Enter' && handleSend()}
+              className='w-full rounded-[20px] bg-white px-5 py-4 pr-32 placeholder:text-sm placeholder:text-[#0000004D] focus:outline-none focus:ring-2 focus:ring-primary'
+              placeholder={t('chatPage.inputPlaceholder')}
+            />
 
-      <div className='flex px-4 pb-8 md768:px-6 md:px-12'>
-        <div className='relative flex-1'>
-          <input
-            value={input}
-            onChange={e => setInput(e.target.value)}
-            onKeyDown={e => e.key === 'Enter' && handleSend()}
-            className='w-full rounded-[20px] bg-white px-5 py-4 pr-32 placeholder:text-sm placeholder:text-[#0000004D] focus:outline-none focus:ring-2 focus:ring-primary'
-            placeholder={t('chatPage.inputPlaceholder')}
-          />
+            <div className='absolute right-4 top-1/2 flex -translate-y-1/2 items-center gap-2'>
+              <button type='button' className='text-gray-500'>
+                <Paperclip size={20} strokeWidth={1.5} />
+              </button>
 
-          <div className='absolute right-4 top-1/2 flex -translate-y-1/2 items-center gap-2'>
-            <button type='button' className='text-gray-500'>
-              <Paperclip size={20} strokeWidth={1.5} />
-            </button>
-
-            <Button
-              onClick={handleSend}
-              className='flex items-center gap-2 rounded-[20px] px-3 py-1 md768:px-5 md768:py-2'
-            >
-              {t('chatPage.send')}
-              <Send size={16} strokeWidth={1.5} />
-            </Button>
+              <Button
+                onClick={handleSend}
+                className='flex items-center gap-2 rounded-[20px] px-3 py-1 md768:px-5 md768:py-2'
+              >
+                {t('chatPage.send')}
+                <Send size={16} strokeWidth={1.5} />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AccessGuard>
   )
 }
--- src/app/(root)/chats/layout.tsx
+++ src/app/(root)/chats/layout.tsx
@@ -13,25 +13,28 @@ export default function Layout({
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [slugValue, setSlugValue] = useState('')
   return (
-    // <AccessGuard requireAuth>
-    <SidebarProvider>
-      <ChatSidebar
-        onOpenModal={(slug: string) => {
-          setSlugValue(slug)
-          setIsModalOpen(true)
-        }}
-      />
-      <SidebarTrigger />
-      <div
-        className='flex min-h-screen w-full flex-1 bg-cover bg-fixed bg-center bg-no-repeat'
-        style={{ backgroundImage: "url('/images/bg-chat.jpg')" }}
-      >
-        {children}
-      </div>
-      {isModalOpen && (
-        <CreateChatModal categorySlug={slugValue} onClose={() => setIsModalOpen(false)} />
-      )}
-    </SidebarProvider>
-    // </AccessGuard>
+    <AccessGuard requireAuth>
+      <SidebarProvider>
+        <ChatSidebar
+          onOpenModal={(slug: string) => {
+            setSlugValue(slug)
+            setIsModalOpen(true)
+          }}
+        />
+        <SidebarTrigger />
+        <div
+          className='flex min-h-screen w-full flex-1 bg-cover bg-fixed bg-center bg-no-repeat'
+          style={{ backgroundImage: "url('/images/bg-chat.jpg')" }}
+        >
+          {children}
+        </div>
+        {isModalOpen && (
+          <CreateChatModal
+            categorySlug={slugValue}
+            onClose={() => setIsModalOpen(false)}
+          />
+        )}
+      </SidebarProvider>
+    </AccessGuard>
   )
 }
