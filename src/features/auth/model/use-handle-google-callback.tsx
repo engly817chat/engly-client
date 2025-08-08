aa8935b4adeae367912416b0ad31d271f95f3a41
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-08-08 12:50:36 +0300

    refactor(auth): update Google callback to fetch access token from backend instead of URL

--- src/features/auth/model/use-handle-google-callback.tsx
+++ src/features/auth/model/use-handle-google-callback.tsx
@@ -3,40 +3,36 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { authApi, useAuth } from '@/entities/auth'
-import { useQueryParams } from '@/shared/hooks/useQueryParams'
-import { saveTokenStorage } from '@/shared/utils'
+import { refreshTokens } from '@/shared/api/refresh'
 
 export function useHandleGoogleCallback() {
-  const params = useQueryParams()
-  const accessToken = params?.get('access_token')
   const router = useRouter()
   const { setUser } = useAuth()
 
   useEffect(() => {
-    if (!params) return
+    const handleGoogleAuth = async () => {
+      try {
+        const { access_token } = await refreshTokens()
 
-    if (accessToken) {
-      saveTokenStorage(accessToken)
+        if (!access_token) {
+          throw new Error('Access token not received')
+        }
 
-      authApi
-        .getProfile()
-        .then(user => {
-          setUser(user)
-          return authApi.isFirstLogin()
-        })
-        .then(response => {
-          if (!response.userExists) {
-            router.push('/google-auth/additional-info')
-          } else {
-            router.push('/')
-          }
-        })
-        .catch(error => {
-          console.error('Error during Google callback flow:', error)
-          router.push('/login')
-        })
-    } else {
-      router.push('/login')
+        const user = await authApi.getProfile()
+        setUser(user)
+
+        const response = await authApi.isFirstLogin()
+        if (!response.userExists) {
+          router.push('/google-auth/additional-info')
+        } else {
+          router.push('/')
+        }
+      } catch (error) {
+        console.error('Error during Google callback flow:', error)
+        router.push('/login')
+      }
     }
-  }, [params, accessToken, router, setUser])
+
+    handleGoogleAuth()
+  }, [router, setUser])
 }
