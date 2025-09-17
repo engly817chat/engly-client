a5e968ee7e00f77f29140c78843dd81ece2c296f
Aleksandra <zubarevaoleksandra05@gmail.com>
2025-09-17 22:42:46 +0300

    feat: make description field in create-chat-modal required

--- src/features/chats/lib/schema.ts
+++ src/features/chats/lib/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const CreateChatSchema = (t: (key: string) => string) =>
   z.object({
     name: z.string().min(1, t('auth.validation.required')),
-    description: z.string().optional(),
+    description: z.string().min(1, t('auth.validation.required')),
   })
 
 export type CreateChatFormValues = z.infer<ReturnType<typeof CreateChatSchema>>
